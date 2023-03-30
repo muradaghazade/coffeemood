baadd3d1b64a3129c0ee974d84fe69cea830188a
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2023-03-30 12:18:15 +0400

    m

--- coffeemood/settings.py
+++ coffeemood/settings.py
@@ -129,9 +129,9 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_DIR = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_URL = '/media/'
